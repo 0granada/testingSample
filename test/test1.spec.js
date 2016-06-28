68c4cfdc41adfa0889ef3d9835b10c77e87068f9
Oscar Granada <ogranada@xumak.com>
2016-06-28 10:22:53 -0500

    First commit

---
+++ test/test1.spec.js
@@ -0,0 +1,13 @@
+
+var chai = require('chai');
+var expect = chai.expect;
+
+
+describe('sample test', function(){
+  it('2+4 will be 6', function(){
+    expect(2+4).to.be.equal(6);
+  });
+});
+
+
+
